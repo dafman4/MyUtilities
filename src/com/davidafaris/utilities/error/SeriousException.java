7baf92365202649e9ebd4ddbd56a638c183f9033
David <David@Davids-Desktop>
2018-12-22 17:25:41 -0600

    updated header, package, and javadoc description of serious exception

--- src/com/davidafaris/utilities/error/SeriousException.java
+++ src/com/davidafaris/utilities/error/SeriousException.java
@@ -1,14 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
- * This is a serious exception at runtime that cannot be easily fixed, and requires a restart or shutdown
+ * 
  */
 package com.davidafaris.utilities.error;
-
+/**
+ * This is a serious exception at runtime that cannot be easily fixed, and requires a restart or shutdown
+ * @author David
+ */
 public class SeriousException extends RuntimeException{
 	private final static String ADDON = "\nTHIS IS A SERIOUS EXCEPTION, shutting down";
 	
